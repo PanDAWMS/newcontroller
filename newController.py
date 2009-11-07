25b76e16be0b3e0961801ed35ac6a9f270980a94
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2009-11-07 05:06:55 +0000

    Starting BDII integration

--- newController.py
+++ newController.py
@@ -515,6 +515,8 @@ if __name__ == "__main__":
 	if cloudd.has_key(None):
 		cloudd[ndef]=cloudd.pop(None)
 
+	bdiiIntegrator(confd, cloudd)
+
 
 	# Create the config path for each cloud
 	for cloud in cloudd:
