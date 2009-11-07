a01080260b42c4344651f02a824e90f39d43b301
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2009-11-07 03:35:24 +0000

    *** empty log message ***

--- newController.py
+++ newController.py
@@ -283,15 +283,17 @@ def composeFields(d,s,dname):
 	# "dname" is one of two things -- "Parameters" or "Override", depending on what part of the  
 	# file we're writing. They're defined generally as param and over 
 	keylist = d[dname].keys()
-	print len(keylist)
+	if 'nickname' not in keylist:
+		print keylist, d
 	try:
 		# Remove the DB key and put in as the first parameter -- this will be "nickname", usually.
 		if dname == param:
 			keylist.remove(dbkey)
 			keylist.sort()
 			keylist.insert(0,dbkey)
-		else:
+ 		else:
 			keylist.sort()
+		print keylist, d
 			
 		
 	# Unless it's not present -- then we'll just throw a warning.	 
