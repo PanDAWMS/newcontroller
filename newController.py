6ac3b089f0a4769a8fbddb0a08272282d177768f
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2009-11-07 03:16:03 +0000

    *** empty log message ***

--- newController.py
+++ newController.py
@@ -283,6 +283,7 @@ def composeFile(d,s,dname):
 	# "dname" is one of two things -- "Parameters" or "Override", depending on what part of the  
 	# file we're writing. They're defined generally as param and over 
 	keylist = d[dname].keys()
+	print keylist
 	try:
 		# Remove the DB key and put in as the first parameter -- this will be "nickname", usually.
 		keylist.remove(dbkey)
@@ -291,7 +292,7 @@ def composeFile(d,s,dname):
 		
 	# Unless it's not present -- then we'll just throw a warning.	 
 	except ValueError:
-		if dname == param: print 'DB key %s not present in this dictionary. Going to be hard to insert. %s' % (dbkey, str(d))
+		#if dname == param: print 'DB key %s not present in this dictionary. Going to be hard to insert. %s' % (dbkey, str(d))
 		keylist.sort()
 
 	# So we're writing a  "Parameters" or "Override" dictionary (dname)...
