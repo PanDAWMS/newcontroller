fc26f7f7753b143bb07371f61faff6eb97eee57d
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2009-11-07 04:22:20 +0000

    *** empty log message ***

--- newController.py
+++ newController.py
@@ -464,12 +464,11 @@ def buildDict():
 				# Feed in the configuration
 				confd[cloud][site][queue][param] = locvars[param]
 				confd[cloud][site][queue][source] = dict([(key,'Config') for key in locvars[param] if key not in excl]) 				
-				if queue is not All:
-					try:
-						confd[cloud][site][queue][over] = locvars[over] 
-						confd[cloud][site][queue][enab] = locvars[enab]
-					except KeyError:
-						print cloud, site, queue
+				print cloud, site, queue, confd[cloud][site][queue], locvars
+				confd[cloud][site][queue][over] = locvars[over] 
+				confd[cloud][site][queue][enab] = locvars[enab]
+				
+
 	# Leaving the All parameters unincorporated
 	os.chdir(base)
 	return confd
