1bb67353e24dbbe550131eb3814272ec7d2bc82f
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-01-09 16:34:45 +0000

    *** empty log message ***

--- configFileHandling.py
+++ configFileHandling.py
@@ -55,7 +55,7 @@ def allMaker(d):
 						d[cloud][site][queue][source][key] = 'the site All.py file for the %s site' % site
 				# Adding the "All" queue to the site
 				d[cloud][site][All] = {param:all_d[cloud][site]}
-				d[cloud][site][All].update({over:{}})
+				d[cloud][site][All].update({over:d[cloud][site][All][over].copy()})
 
 	return 0
 
