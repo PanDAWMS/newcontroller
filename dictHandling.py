0f36145a9e0cf0ab33e1fb766f003d659c9c38fa
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-01-09 16:24:56 +0000

    *** empty log message ***

--- dictHandling.py
+++ dictHandling.py
@@ -144,7 +144,11 @@ def buildDict():
 				# Feed in the configuration
 				confd[cloud][site][queue][param] = locvars[param]
 				confd[cloud][site][queue][over] = locvars[over] 
-				if queue is not All: confd[cloud][site][queue][enab] = locvars[enab]
+				try:
+					if queue != All: confd[cloud][site][queue][enab] = locvars[enab]
+				except KeyError:
+					print cloud, site, queue, q, confd[cloud][site][queue]
+					print '\n',locvars
 				confd[cloud][site][queue][source] = dict([(key,'Config') for key in locvars[param] if key not in excl]) 				
 				
 	# Leaving the All parameters unincorporated
