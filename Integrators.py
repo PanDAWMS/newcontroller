4400c55cc640ec7fb3c10a8953f4b2d3a394df2a
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-02-15 22:46:20 +0000

    *** empty log message ***

--- Integrators.py
+++ Integrators.py
@@ -256,6 +256,15 @@ def bdiiIntegrator(confd,d):
 				confd[c][s][nickname][enab] = 'False'
 				# Either way, we need to put the queue in without a cloud defined. 
 		# If the queue doesn't actually exist, we need to add it.
+		if c not in confd:
+			if bdiiDebug: print 'Creating cloud %s' % c
+			confd[c]={}
+		# If site doesn't yet exist, create it:
+		if s not in confd[c]:
+			if bdiiDebug: print 'Creating site %s in cloud %s as requested by BDII'  % (s,c)
+			confd[c][s] = {}
+			# Create it in the main config dictionary, using the standard keys from the DB (set in the initial import)
+		print 'Creating queue %s in site %s and cloud %s as requested by BDII. This queue must be enabled by hand.' % (nickname, s, c)
 		if not confd[c][s].has_key(nickname): confd[c][s][nickname] = protoDict(nickname,{},sourcestr='Queue created by BDII',keys=standardkeys)
 		# Check for manual setting. If it's manual, DON'T TOUCH
 		if confd[c][s][nickname][param].has_key('sysconfig'):
