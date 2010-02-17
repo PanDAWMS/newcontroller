7ba8e7337750545dd5bee79290467cecaf0d5b11
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-02-17 15:07:57 +0000

    Comments

--- controllerSettings.py
+++ controllerSettings.py
@@ -8,7 +8,7 @@
 import os
 
 # Debug Flags
-genDebug = True
+genDebug = False
 toaDebug = False
 jdlDebug = False
 bdiiDebug = False
@@ -18,23 +18,36 @@ configReadDebug = False
 configWriteDebug = False
 
 # Global strings and lists
+
+# If safety is on, nothing is written to the DB.
 safety = 'off'
-All = 'All'
-ndef = 'Deactivated'
-param = 'Parameters'
-over = 'Override'
-jdl = 'JDL'
-source = 'Source'
-enab = 'Enabled'
+
+# Widely used strings
+
+All = 'All' # Standard name for All files
+ndef = 'Deactivated' # Standard name for cloud=NULL queues
+param = 'Parameters' # Name for the parameters dictionary in a queue spec
+over = 'Override' # Name for the override dictionary in a queue spec
+jdl = 'JDL' # The parameters dictionary for a JDL specification (no need for override)
+source = 'Source' # The name for a sources dictionary (names the provenance of a setting in the parameters dictionary for a queue
+enab = 'Enabled' # Specifies that a queue has been enabled.
+
+# Sets the present path as the primary -- allows portability, but the script has to be run from its home directory.
 base_path = os.getcwd()
+
 # Step back a layer in the path for the configs, and put them in the config SVN directory
 cfg_path = base_path[:base_path.rfind(os.sep)] + os.sep + 'pandaconf' + os.sep
+# Paths for backup files
 backupPath = cfg_path + 'Backup'
 backupName = 'schedConfigBackup.pickle'
+# Config file path specifications
 configs = cfg_path + os.sep + 'SchedConfigs'
 jdlconfigs = cfg_path + os.sep + 'JDLConfigs'
 postfix = '.py'
+# jdlkey is the jdllist table primary key, and dbkey is the schedconfig table primary key.
 jdlkey, dbkey, dsep, keysep, pairsep, spacing = 'name', 'nickname', ' : ', "'", ',', '    '  # Standard python spacing of 4
 shared, unshared = 'shared','unshared'
+# These are the DB fields that should never be modified by the controller -- fixed by hand using curl commands.
 excl = ['status','lastmod','dn','tspace','_comment']
+# This list is global, and is populated (during initial DB import) by the list of columns that the schedconfig table contains. 
 standardkeys = []
