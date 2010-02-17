fb19b8f7fe688574f3b19f8f3dad5dbdbef9642a
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-02-17 19:21:39 +0000

    Debug

--- svnHandling.py
+++ svnHandling.py
@@ -14,28 +14,34 @@ from controllerSettings import *
 
 def svnCheckout():
 	''' Check out the configuration SVN -- should only be necessary on rare occasions.'''
+	if svnDebug: print 'Beginning SVN checkout'
 	path = os.getcwd()
 	os.chdir(cfg_path)
 	print '####### Checking out the SVN repository anew -- this should be a RARE event! Is this really what you want to do? #############'
 	os.system('svn co %s' % confrepo)
 	os.chdir(path)
+	if svnDebug: print 'Completing SVN checkout'
 	return 0
 
 def svnCheckin(notestr = ''):
 	''' Update the SVN repo with changes from BDII and ToA. Argument, if provided, notes which files received updates. '''
+	if svnDebug: print 'Beginning SVN checkin'
 	path = os.getcwd()
-	os.chdir(cfg_path)
+	os.chdir(cfg_path + os.sep + )
 	# Timestamp in GMT
 	message = 'Changes made: %s%s' % (time.asctime(time.gmtime()),notestr)
 	os.system('svn ci %s -m "%s"' % (confrepo, message))
 	os.chdir(path)
+	if svnDebug: print 'Completing SVN checkin'
 	return 0
 
 def svnUpdate():
 	''' Update from the SVN repo -- introduce changes from user configs. '''
+	if svnDebug: print 'Beginning SVN checkin'
 	path = os.getcwd()
 	os.chdir(cfg_path)
 	os.system('svn up')
 	os.chdir(path)
+	if svnDebug: print 'Completing SVN update'
 	return 0
 
