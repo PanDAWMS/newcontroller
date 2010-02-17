06f3c47c31b445dcb78dbf7e72b9533e1554e8b9
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-02-17 16:54:19 +0000

    SVN methods for config file handling

---
+++ svnHandling.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+#######################################################
+# Handles storage and modification of queue data      #
+# in the ATLAS PanDA schedconfig and jdllist tables   #
+# for ATLAS production and analysis queues.           #
+#                                                     #
+# Alden Stradling (Alden.Stradling@cern.ch) 23 Jun 09 #
+#######################################################
+
+import os, sys, commands, time
+
+from controllerSettings import *
+
+def svnCheckout():
+	''' Check out the configuration SVN -- should only be necessary on rare occasions.'''
+	path = os.getcwd()
+	os.chdir(cfg_path)
+	print '####### Checking out the SVN repository anew -- this should be a RARE event! Is this really what you want to do? #############'
+	os.system('svn co %s' % configrepo)
+	os.chdir(path)
+	return 0
+
+def svnUpdate(notestr = ''):
+	''' Update the SVN repo with changes from BDII and ToA. Argument, if provided, notes which files received updates. '''
+	path = os.getcwd()
+	os.chdir(cfg_path)
+	# Timestamp in GMT
+	message = 'Changes made: %s%s' % (time.asctime(time.gmtime(),notestr))
+	os.system('svn ci %s -m "%s"' % (configrepo, message))
+	os.chdir(path)
+	return 0
+	
