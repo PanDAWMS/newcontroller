617ed3875dfe28f80f8f51727b8245107daf3e90
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-04-14 16:23:54 +0000

    *** empty log message ***

--- newController.py
+++ newController.py
@@ -63,8 +63,8 @@ def compDictColl(d1,d2,exclList=[]):
 				if d1[queue][i] != d2[queue][i]:
 					if i not in exclList:
 						print i, d1[queue][i], d2[queue][i], type(d1[queue][i]), type(d2[queue][i])	
-				except KeyError:
-					print 'No key %s in %s %s %s' % (i, queue)
+			except KeyError:
+				print 'No key %s in %s %s %s' % (i, queue)
 					
 					
 def loadJdl():
