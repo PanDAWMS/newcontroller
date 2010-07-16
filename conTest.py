9526df4cb8e4da2eeb676502d221105e0fea6488
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-07-16 12:36:05 +0000

    *** empty log message ***

--- conTest.py
+++ conTest.py
@@ -24,88 +24,88 @@ def loadConfigs():
 	'''Run the schedconfig table updates'''
 	
 	# Load the database as it stands as a primary reference
-	#dbd, standardkeys = sqlDictUnpacker(loadSchedConfig())
+	dbd, standardkeys = sqlDictUnpacker(loadSchedConfig())
 
-## 	#Update the local configuration files from SVN
-## 	svnUpdate()
+	#Update the local configuration files from SVN
+	svnUpdate()
 	
-## 	# Load the present config files, based on the SVN update
-## 	configd = buildDict()
+	# Load the present config files, based on the SVN update
+	configd = buildDict()
 
-## 	# Load the JDL from the DB and from the config files, respectively
-## 	jdldb, jdldc = loadJdl()
+	# Load the JDL from the DB and from the config files, respectively
+	jdldb, jdldc = loadJdl()
 
-## 	# Add the BDII information, and build a list of releases
-## ## 	old_rel_db = loadInstalledSW()
-##  	new_rel_db = {}
-##  	bdiiIntegrator(configd, new_rel_db, dbd)
-## 	# Check the old DB for releases to delete, and the new one for releases to insert.
-## ## 	delete_sw = [old_rel_db[i] for i in old_rel_db if i not in new_rel_db]
-## ## 	insert_sw = [new_rel_db[i] for i in new_rel_db if i not in old_rel_db]
+	# Add the BDII information, and build a list of releases
+## 	old_rel_db = loadInstalledSW()
+ 	new_rel_db = {}
+ 	bdiiIntegrator(configd, new_rel_db, dbd)
+	# Check the old DB for releases to delete, and the new one for releases to insert.
+## 	delete_sw = [old_rel_db[i] for i in old_rel_db if i not in new_rel_db]
+## 	insert_sw = [new_rel_db[i] for i in new_rel_db if i not in old_rel_db]
 
-## 	# Now add ToA information
-## 	toaIntegrator(configd)
+	# Now add ToA information
+	toaIntegrator(configd)
 	
-## 	# Compose the "All" queues for each site
-## 	status = allMaker(configd)
+	# Compose the "All" queues for each site
+	status = allMaker(configd)
 
-## 	# Compare the DB to the present built configuration to find the queues that are changed.
-## 	up_d, del_d = compareQueues(collapseDict(dbd), collapseDict(configd), dbOverride)
-## 	jdl_up_d, jdl_del_d = compareQueues(jdldb, jdldc, dbOverride)
-## 	deletes = [del_d[i][dbkey] for i in del_d]
+	# Compare the DB to the present built configuration to find the queues that are changed.
+	up_d, del_d = compareQueues(collapseDict(dbd), collapseDict(configd), dbOverride)
+	jdl_up_d, jdl_del_d = compareQueues(jdldb, jdldc, dbOverride)
+	deletes = [del_d[i][dbkey] for i in del_d]
 
-## 	# Delete queues that are not Enabled
-## 	del_d.update(disabledQueues(configd))
+	# Delete queues that are not Enabled
+	del_d.update(disabledQueues(configd))
 
-## 	# Get the database updates prepared for insertion.
-## 	# The Delete list is just a list of SQL commands (don't add semicolons!)
-## 	del_l = buildDeleteList(del_d,'schedconfig')
+	# Get the database updates prepared for insertion.
+	# The Delete list is just a list of SQL commands (don't add semicolons!)
+	del_l = buildDeleteList(del_d,'schedconfig')
 
-## 	# The other updates are done using the standard replaceDB method from the SchedulerUtils package.
-## 	# The structure of the list is a list of dictionaries containing column/value as the key/value pairs.
-## 	# The primary key is specified for the replaceDB method. For schedconfig, it's dbkey, and for the jdls it's jdlkey
-## 	# (specified in controllerSettings
-## 	up_l = buildUpdateList(up_d,param)
-## 	jdl_l = buildUpdateList(jdl_up_d,jdl)
+	# The other updates are done using the standard replaceDB method from the SchedulerUtils package.
+	# The structure of the list is a list of dictionaries containing column/value as the key/value pairs.
+	# The primary key is specified for the replaceDB method. For schedconfig, it's dbkey, and for the jdls it's jdlkey
+	# (specified in controllerSettings
+	up_l = buildUpdateList(up_d,param)
+	jdl_l = buildUpdateList(jdl_up_d,jdl)
 
-## 	# If the safety is off, the DB update can continue
-## 	if safety is not 'on':
-## 		utils.initDB()
-## 		# Individual SQL statements to delete queues that need deleted
-## 		for i in del_l:
+	# If the safety is off, the DB update can continue
+	if safety is not 'on':
+		utils.initDB()
+		# Individual SQL statements to delete queues that need deleted
+		for i in del_l:
+			try:
+				utils.dictcursor().execute(i)
+			except:
+				print 'Failed SQL Statement: ', i
+
+		# Schedconfig table gets updated all at once
+		print 'Updating SchedConfig'
+		utils.replaceDB('schedconfig',up_l,key=dbkey)
+		# Jdllist table gets updated all at once
+		print 'Updating JDLList'
+		utils.replaceDB('jdllist',jdl_l,key=jdlkey)
+		# Changes committed after all is successful, to avoid partial updates
+		utils.commit()
+		# FIX This string will eventually be filled with changed queue names and other info for the subversion checkin
+		svnstring=''
+
+		# Delete outdated installedsw entries
+## 		for i in delete_sw:
 ## 			try:
-## 				utils.dictcursor().execute(i)
+## 				sql = "DELETE FROM installedsw WHERE release = '%s' and cache = '%s' and siteid = '%s'" % (i['release'],i['cache'],i['siteid'])
+## 				utils.dictcursor().execute(sql)
 ## 			except:
-## 				print 'Failed SQL Statement: ', i
+## 				print 'Failed SQL Statement: ', sql, i
 
-## 		# Schedconfig table gets updated all at once
-## 		print 'Updating SchedConfig'
-## 		utils.replaceDB('schedconfig',up_l,key=dbkey)
-## 		# Jdllist table gets updated all at once
-## 		print 'Updating JDLList'
-## 		utils.replaceDB('jdllist',jdl_l,key=jdlkey)
-## 		# Changes committed after all is successful, to avoid partial updates
+## 		# Insert new installedsw entries. Not using replaceDB because of a lack of single DB key.
+## 		for i in insert_sw:
+## 			try:
+## 				sql = "INSERT INTO installedsw (release, cache, siteid, cloud) values ('%s','%s','%s','%s')" % (i['release'],i['cache'],i['siteid'],i['cloud'])
+## 				utils.dictcursor().execute(sql)
+## 			except:
+## 				print 'Failed SQL Statement: ', sql, i
+## 		# Commit installedsw changes
 ## 		utils.commit()
-## 		# FIX This string will eventually be filled with changed queue names and other info for the subversion checkin
-## 		svnstring=''
-
-## 		# Delete outdated installedsw entries
-## ## 		for i in delete_sw:
-## ## 			try:
-## ## 				sql = "DELETE FROM installedsw WHERE release = '%s' and cache = '%s' and siteid = '%s'" % (i['release'],i['cache'],i['siteid'])
-## ## 				utils.dictcursor().execute(sql)
-## ## 			except:
-## ## 				print 'Failed SQL Statement: ', sql, i
-
-## ## 		# Insert new installedsw entries. Not using replaceDB because of a lack of single DB key.
-## ## 		for i in insert_sw:
-## ## 			try:
-## ## 				sql = "INSERT INTO installedsw (release, cache, siteid, cloud) values ('%s','%s','%s','%s')" % (i['release'],i['cache'],i['siteid'],i['cloud'])
-## ## 				utils.dictcursor().execute(sql)
-## ## 			except:
-## ## 				print 'Failed SQL Statement: ', sql, i
-## ## 		# Commit installedsw changes
-## ## 		utils.commit()
 
 
 
