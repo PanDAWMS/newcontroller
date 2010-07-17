3ab65a269ffa7d488f962520b978004111fe0b31
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-07-17 01:26:02 +0000

    *** empty log message ***

--- miscUtils.py
+++ miscUtils.py
@@ -87,7 +87,7 @@ def testDiff(mm,nn):
 				if k not in ['releases']:
 					if mm[i][k] != nn[i][k]:
 						if k=='jdladd':
-							if mm[i][k].strip('Queue').strip() != nn[i][k].strip('Queue').strip():
+							if mm[i][k][:mm[i][k].find('Queue')].strip() != nn[i][k][:nn[i][k].find('Queue')].strip():
 								print i, k, mm[i][k], nn[i][k], type(mm[i][k]), type(nn[i][k])
 						else:
 							print i, k, mm[i][k], nn[i][k], type(mm[i][k]), type(nn[i][k])
