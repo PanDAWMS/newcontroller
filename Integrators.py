56f1e55e4e7c8bc36630fa88f270b92d499116dd
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-07-21 18:09:40 +0000

    Header added

--- Integrators.py
+++ Integrators.py
@@ -1,3 +1,11 @@
+#! /usr/bin/env python
+#######################################################
+# Handles modification of queue data based on ToA and #
+# BDII inputs.                                        #
+# Based largely on existing code, cleaned up some.    #
+# Alden Stradling (Alden.Stradling@cern.ch) 23 Jun 09 #
+#######################################################
+
 import os, commands, re
 
 from miscUtils import *
