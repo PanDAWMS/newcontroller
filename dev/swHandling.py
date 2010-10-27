b09450f09a8c0d15db2ded2df116e7ef62be1620
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-10-27 22:09:54 +0000

    site->siteid

--- dev/swHandling.py
+++ dev/swHandling.py
@@ -67,25 +67,25 @@ def updateInstalledSW(confd,lcgdict):
 				# ASSUMPTION -- that base releases will always contain two periods as separators
 				release=baseReleaseSep.join(cache.split('-')[1].split(baseReleaseSep)[:nBaseReleaseSep])
 				# The unique name for this entry as a tuple
-				index = (site,release,cache)
+				index = (siteid,release,cache)
 				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':release,'cache':cache}
 				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':release,'cache':None}
 			# If the Conditions meta-tag is present, create an entry for it in the dictionary
 			if Conditions in release_tags[gatekeeper[queue]]:
 				# The unique name for this entry as a tuple
-				index = (site,Conditions,Conditions)
+				index = (siteid,Conditions,Conditions)
 				# Adding the Conditions meta-tag from the caches dictionary
 				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':Conditions,'cache':Conditions}
 
 		if release_tags.has_key(gatekeeper[queue]):
 			for release in release_tags[gatekeeper[queue]]:
 				# The unique name for this entry as a tuple
-				index = (site,release,None)
+				index = (siteid,release,None)
 				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':release,'cache':None}
 			# If the Conditions meta-tag is present, create an entry for it in the dictionary
 			if Conditions in release_tags[gatekeeper[queue]]:
 				# The unique name for this entry as a tuple
-				index = (site,Conditions,Conditions)
+				index = (siteid,Conditions,Conditions)
 				# Adding the Conditions meta-tag from the releases dictionary (redundant, but that's fine)
 				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':Conditions,'cache':Conditions}
 	
