9ada43c387a98f06549d0b71cc21edacab982d25
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-11-01 22:57:36 +0000

    *** empty log message ***

--- dev/swHandling.py
+++ dev/swHandling.py
@@ -49,7 +49,7 @@ def updateInstalledSW(confd,lcgdict):
 				# and make sure you split off the non-gatekeeper-name part at the end.
 				gatekeeper[queue] = confd[queue]['queue'].split('/')[0]
 				print 'Other Gatekeeper for %s is %s' % (queue,confd[queue]['queue'].split('/')[0])
-		else: print 'Skipping %s' % queue
+		else: print 'Skipping %s with siteid %s' % (queue,confd[queue]['siteid'])
 	# Time to build the master list from BDII:
 
 	# The values will be de-duplicated in a dictionary. Keys will be (siteid,release,queue) together in a tuple
