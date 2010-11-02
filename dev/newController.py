221001fb1d275526e7119cba7d0041332805e7ee
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-11-02 01:12:23 +0000

    *** empty log message ***

--- dev/newController.py
+++ dev/newController.py
@@ -144,8 +144,10 @@ def loadConfigs():
 			
 	# Check out the db as a new dictionary
 	newdb, sk = sqlDictUnpacker(loadSchedConfig())
-	if not bdiiOverride: updateInstalledSW(collapseDict(newdb),linfotool)
-	
+	if not bdiiOverride:
+		if genDebug: sw_db, sw_bdii, delList, addList, confd, cloud, siteid, gk=updateInstalledSW(collapseDict(newdb),linfotool)
+		else: updateInstalledSW(collapseDict(newdb),linfotool)
+		
 	# If the checks pass (no difference between the DB and the new configuration)
 	checkUp, checkDel = compareQueues(collapseDict(newdb), collapseDict(configd))
 	
@@ -157,7 +159,7 @@ def loadConfigs():
 	backupCreate(newdb)
 
 	# For development purposes, we can get all the important variables out of the function. Usually off.
-	if genDebug: return linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel
+	if genDebug: return sw_db, sw_bdii, delList, addList, confd, cloud, siteid, gk, linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel
 	return 0
 	
 
