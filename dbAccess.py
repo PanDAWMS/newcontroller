1259294fc4fccdd9dee3bf8eb50153538095272e
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-11-02 21:52:32 +0000

    *** empty log message ***

--- dbAccess.py
+++ dbAccess.py
@@ -66,10 +66,14 @@ def updateInstalledSWdb(addList, delList):
 	print "Init DB"
 	for i in addList:
 		sql="INSERT INTO installedsw (SITEID,CLOUD,RELEASE,CACHE) VALUES ('%s','%s','%s','%s')" % (i['siteid'],i['cloud'],i['release'],i['cache'])
+		print sql
 		utils.dictcursor().execute(sql)
+		
 	for i in delList:
 		sql="DELETE FROM installedsw WHERE siteid = '%s' and release = '%s' and cache = '%s'" % (i['siteid'],i['release'],i['cache'])
+		print sql
 		utils.dictcursor().execute(sql)
+		
 	utils.commit
 	utils.endDB()
 
