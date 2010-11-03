25720519b59a13636be66478b58e55cb922a81d0
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-11-03 16:02:48 +0000

    *** empty log message ***

--- dev/swHandling.py
+++ dev/swHandling.py
@@ -76,8 +76,8 @@ def updateInstalledSW(confd,lcgdict):
 	
 	deleteList = [sw_db[i] for i in sw_db if i not in sw_bdii]
 	addList = [sw_bdii[i] for i in sw_bdii if i not in sw_db]
-	deleteDB = [sw_db[i] for i in sw_db if i not in sw_bdii]
-	addDB = [sw_bdii[i] for i in sw_bdii if i not in sw_db]
+	deleteDB = dict([(i, sw_db[i]) for i in sw_db if i not in sw_bdii])
+	addDB = dict([(i, sw_bdii[i]) for i in sw_bdii if i not in sw_db])
 	
 	try:
 		updateInstalledSWdb(addList,deleteList)
