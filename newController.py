6f1532ad5863db633888f210bc0be79eda298bdf
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-11-12 23:54:26 +0000

    Fixes to cloud autosetting

--- newController.py
+++ newController.py
@@ -79,7 +79,6 @@ def loadConfigs():
 		linfotool = lcgInfositeTool.lcgInfositeTool()
 	
 	# Now add ToA information
-	# Tiers of Atlas has not been updating in a long time. Need to fix
 	if not toaOverride: toaIntegrator(configd)
 	
 	# Compose the "All" queues for each site
@@ -143,7 +142,7 @@ def loadConfigs():
 	# Check out the db as a new dictionary
 	newdb, sk = sqlDictUnpacker(loadSchedConfig())
  	if not bdiiOverride:
- 		if genDebug: sw_db, sw_bdii, delList, addList, delDB, addDB, confd, cloud, siteid, gk=updateInstalledSW(collapseDict(newdb),linfotool)
+ 		if genDebug: sw_db, sw_bdii, delList, addList, confd, cloud, siteid, gk=updateInstalledSW(collapseDict(newdb),linfotool)
  		else: updateInstalledSW(collapseDict(newdb),linfotool)
 		
 	# If the checks pass (no difference between the DB and the new configuration)
@@ -157,7 +156,7 @@ def loadConfigs():
 	backupCreate(newdb)
 
 	# For development purposes, we can get all the important variables out of the function. Usually off.
-	if genDebug: return sw_db, sw_bdii, delList, addList, delDB, addDB, confd, cloud, siteid, gk, linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel
+	if genDebug: return sw_db, sw_bdii, delList, addList, confd, cloud, siteid, gk, linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel
 	return 0
 	
 
@@ -182,7 +181,7 @@ if __name__ == "__main__":
 	# All of the passed dictionaries will be eliminated at the end of debugging. Necessary for now.
 	dbd, standardkeys = sqlDictUnpacker(loadSchedConfig())
 
-	if genDebug: sw_db, sw_bdii, delList, addList, delDB, addDB, confd, cloud, siteid, gk, linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel = loadConfigs()
+	if genDebug: sw_db, sw_bdii, delList, addList, confd, cloud, siteid, gk, linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel = loadConfigs()
 	else: loadConfigs()
 
 	#os.chdir(base_path)
