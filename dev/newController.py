cc98922ddc5439e43173ae85ea967f8356bd9cae
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2010-11-12 18:58:03 +0000

    *** empty log message ***

--- dev/newController.py
+++ dev/newController.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
 	# All of the passed dictionaries will be eliminated at the end of debugging. Necessary for now.
 	dbd, standardkeys = sqlDictUnpacker(loadSchedConfig())
 
-	if genDebug: sw_db, sw_bdii, delList, addList, delDB, addDB, confd, cloud, siteid, gk, linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel = loadConfigs()
+	if genDebug: sw_db, sw_bdii, delList, addList, confd, cloud, siteid, gk, linfotool, dbd, configd, up_d, del_d, del_l, up_l, jdl_l, jdldc, newdb, checkUp, checkDel = loadConfigs()
 	else: loadConfigs()
 
 	#os.chdir(base_path)
