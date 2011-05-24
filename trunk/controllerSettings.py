0e06481b05b6848fc8421ee4b277c61fb8d97b12
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2011-05-24 21:20:37 +0000

    *** empty log message ***

--- trunk/controllerSettings.py
+++ trunk/controllerSettings.py
@@ -74,7 +74,7 @@ jdlkey, dbkey, dsep, keysep, pairsep, spacing = 'name', 'nickname', ' : ', "'", 
 shared, unshared = 'shared','unshared'
 
 # These are the DB fields that are required not to be null, along with defaults. 
-nonNull={'name':'default','system':'unknown','site':'?','nqueue':'0','nodes':'0','queuehours':'0','memory':'0', 'maxtime':'0', 'space':'0'}
+nonNull={'name':'default','system':'unknown','site':'?','nqueue':'0','nodes':'0','queuehours':'0','memory':'0', 'maxtime':'0', 'space':'0', 'tspace':'0'}
 
 # These are the DB fields that should never be modified by the controller -- fixed by hand using curl commands.
 excl = ['status','lastmod','dn','tspace','comment_','space','nqueue','nqueues','last_status','sysconfig'] # nqueues takes care of a typo
