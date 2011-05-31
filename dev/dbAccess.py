6daa89788fc77c57a9584178d1ea705f79be378c
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2011-05-31 19:01:10 +0000

    *** empty log message ***

--- dev/dbAccess.py
+++ dev/dbAccess.py
@@ -89,7 +89,7 @@ def updateInstalledSWdb(addList, delList):
 			utils.dictcursor().execute(sql)
 		except:
 			print "SQL failed: %s" % sql 
-			print sys.exc_info
+			print sys.exc_info()
 			
 	for i in delList:
 		sql="DELETE FROM installedsw WHERE siteid = '%s' and release = '%s' and cache = '%s'" % (i['siteid'],i['release'],i['cache'])
