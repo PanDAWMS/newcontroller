1a663868d9413aad6afd015f07adcf84dad30e45
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2011-06-01 23:24:36 +0000

    *** empty log message ***

--- swHandling.py
+++ swHandling.py
@@ -136,6 +136,8 @@ def testSW(confd,lcgdict):
 	# completeness. This is why I just add EVERYTHING and let the keys sort it out.
 	
 	sw_bdii = {}
+
+	
 	
 	for queue in siteid:
 		# Check for the gatekeeper value in the BDII:
@@ -146,6 +148,7 @@ def testSW(confd,lcgdict):
 				release=baseReleaseSep.join(cache[rel].split('-')[1].split(baseReleaseSep)[:nBaseReleaseSep])
 				# The unique name for this entry
 				index = '%s_%s_%s_%s' % (siteid[queue],release,cache[rel],cache[cmt])
+				index = index.replace('None','')
 				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':release,'cache':cache[rel],'cmtConfig':cache[cmt]}
 
 		if release_tags.has_key(gatekeeper[queue]):
@@ -153,6 +156,7 @@ def testSW(confd,lcgdict):
 				cache = 'None'
 				# The unique name for this entry
 				index = '%s_%s_%s_%s' % (siteid[queue],release[rel],cache,release[cmt])
+				index = index.replace('None','')
 				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':release[rel],'cache':cache,'cmtConfig':release[cmt]}
 	
 	unicodeEncode(sw_bdii)
