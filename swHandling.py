880a2866788c4729e2760365e4b8ef7037214d9c
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2011-06-01 21:28:06 +0000

    *** empty log message ***

--- swHandling.py
+++ swHandling.py
@@ -95,3 +95,71 @@ def updateInstalledSW(confd,lcgdict):
 		return sw_db, sw_bdii, deleteList, addList, confd, cloud, siteid, gatekeeper  
 	else:
 		return 0
+
+def testSW(confd,lcgdict):
+	'''Checks for changes to the installedsw table, and add or delete releases as necessary by site'''
+	# Call on the DB to get the present installedsw version. From dbAccess
+	sw_db = loadInstalledSW()
+	# Get the present BDII tags information from the (previously called) lcgInfositeTool2 
+	release_tags = lcgdict.CEtags
+	cache_tags = lcgdict.CEctags
+	siteid = {}
+	gatekeeper = {}
+	cloud = {}
+	# Make any translation necessary to the cache tags (see controllerSettings for more info)
+	translateTags(cache_tags)
+
+	# We now have a full set of lookups. We need to build a list of siteids, gatekeepers and clouds from the config dict:
+	for queue in confd:
+		# If the queue has a siteid, assign it and a gatekeeper. If !siteid, it's deactivated. 
+		if confd[queue].has_key('siteid') and confd[queue]['siteid']:
+			# Collect the cloud information, choosing only principal cloud. Comma-delimited.
+			cloud[queue] = confd[queue]['cloud'].split(',')[0]
+			siteid[queue] = confd[queue]['siteid']
+			if len(cloud[queue]) > 8: print cloud[queue], queue, siteid[queue]
+			# If it's not an analysis queue and has a siteid, use gatekeeper as the BDII key
+			if confd[queue]['gatekeeper'] != virtualQueueGatekeeper:
+				gatekeeper[queue] = confd[queue]['gatekeeper']
+			# If it's an analy queue, use the "queue" value instead
+			elif confd[queue]['queue']:
+				# and make sure you split off the non-gatekeeper-name part at the end.
+				gatekeeper[queue] = confd[queue]['queue'].split('/')[0]
+			# If there's no good gatekeeper information, forget the queue
+			else:
+				cloud.pop(queue)
+				siteid.pop(queue)
+
+	# Time to build the master list from BDII:
+
+	# The values will be de-duplicated in a dictionary. Keys will be (siteid,cloud,release,queue) together in a tuple
+	# I'm not worried about redundant additions (the dictionary will handle that), but I _am_ concerned about
+	# completeness. This is why I just add EVERYTHING and let the keys sort it out.
+	
+	sw_bdii = {}
+	
+	for queue in siteid:
+		# Check for the gatekeeper value in the BDII:
+		if cache_tags.has_key(gatekeeper[queue]):
+			for cache in cache_tags[gatekeeper[queue]]:
+				# Once again -- cache[rel] is cache[0], because the release and cache records are (release,cmt)
+				# ASSUMPTION -- that base releases will always contain two periods as separators
+				release=baseReleaseSep.join(cache[rel].split('-')[1].split(baseReleaseSep)[:nBaseReleaseSep])
+				# The unique name for this entry
+				index = '%s_%s_%s_%s' % (siteid[queue],release,cache[rel],cache[cmt])
+				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':release,'cache':cache[rel],'cmtConfig':cache[cmt]}
+
+		if release_tags.has_key(gatekeeper[queue]):
+			for release in release_tags[gatekeeper[queue]]:
+				cache = 'None'
+				# The unique name for this entry
+				index = '%s_%s_%s_%s' % (siteid[queue],release[rel],cache,release[cmt])
+				sw_bdii[index] = {'siteid':siteid[queue],'cloud':cloud[queue],'release':release[rel],'cache':cache,'cmtConfig':release[cmt]}
+	
+	unicodeEncode(sw_bdii)
+	unicodeEncode(sw_db)
+
+	deleteList = [sw_db[i] for i in sw_db if i not in sw_bdii]
+	addList = [sw_bdii[i] for i in sw_bdii if i not in sw_db]
+	
+	return sw_db, sw_bdii, deleteList, addList, confd, cloud, siteid, gatekeeper  
+
