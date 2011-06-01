f453a9603d51826e4e7ae34139ee18210d880966
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2011-06-01 23:33:47 +0000

    *** empty log message ***

--- dbAccess.py
+++ dbAccess.py
@@ -142,6 +142,7 @@ def testUpdate(addList, delList):
 			sql="DELETE FROM installedsw WHERE siteid = '%s' and release = '%s' and cache is NULL" % (i['siteid'],i['release'])
 			if i['cmtconfig'] is None: sql += " and cmtconfig is NULL"
 			else: sql += " and cmtconfig = '%s'" % i['cmtConfig']
+		print sql
 		utils.dictcursor().execute(sql)
 	utils.commit()
 		
