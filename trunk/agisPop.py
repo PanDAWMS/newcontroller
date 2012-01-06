dcd6911a02b1cf8eb2d6e3252d31723493c2cc1c
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2012-01-06 00:29:02 +0000

    *** empty log message ***

--- trunk/agisPop.py
+++ trunk/agisPop.py
@@ -6,9 +6,28 @@ import pickle
 a=AGIS(hostp='atlas-agis-api-dev.cern.ch:80')
 f=file('installedswdump.p')
 l=pickle.load(f)
+releases=a.list_swreleases()
+
 for i in l:
-    try:
-        r=a.add_swrelease(release=i['cache'].split('-')[1],cmtconfig=i['cmtConfig'],major_release=i['release'],project=i['cache'].split('-')[0])
-    except:
-        pass
-    r=a.add_panda_swrelease(panda_resource=i['siteid'],project=i['cache'].split('-')[0], release=i['cache'].split('-')[1], cmtconfig=i['cmtConfig'],major_release=i['release'])
+	if '-' in i['cache']:
+		release = i['cache'].split('-')[1]
+		project = i['cache'].split('-')[0]
+	elif cache == 'None':
+		release = i['cache']
+		project = i['cache']
+	else:
+		release = i['cache']
+		project = i['cache']
+
+	panda_resource = i['siteid']
+	cmtconfig = i['cmtConfig']
+	major_release = i['release']
+	
+	if releases.has_key(release):
+		if releases[release]['cmtConfig'] == cmtconfig and releases[release]['project'] == project:
+			pass
+		elif not cmtconfig or not project:
+			continue
+		else:
+			r=a.add_swrelease(release, cmtconfig=i['cmtConfig'], major_release=i['release'], project)
+	r=a.add_panda_swrelease(panda_resource=i['siteid'], project, release, cmtconfig=i['cmtConfig'], major_release=i['release'])
