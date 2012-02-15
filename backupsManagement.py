25dee13b539b9e8fd16c6b3db709d839adf9eccc
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2012-02-15 23:45:46 +0000

    *** empty log message ***

--- backupsManagement.py
+++ backupsManagement.py
@@ -8,12 +8,14 @@
 import os, sys
 from controllerSettings import *
 
-hbList = [i for i in os.listdir(hotBackupPath) if volatileCSVName in i or volatileSQLName in i][:hotBackups]
-lbList = [i for i in os.listdir(longBackupPath) if i.endswith('.gz')][:keptBackups]
+hbList = [i for i in sorted(os.listdir(hotBackupPath)) if volatileCSVName in i or volatileSQLName in i][hotBackups:]
+lbList = [i for i in sorted(os.listdir(longBackupPath)) if i.endswith('.gz')][keptBackups:]
 
 for i in hbList:
-	os.system('rm %s' % hotBackupPath + i)
+	#os.system('rm %s' % hotBackupPath + i)
+	print('rm %s' % hotBackupPath + i)
 for i in lbList:
-	os.system('rm %s' % longBackupPath + i)
+	#os.system('rm %s' % longBackupPath + i)
+	print('rm %s' % longBackupPath + i)
 
 
