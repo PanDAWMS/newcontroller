4283c1161e9785e100a0bbfc59e9bb537408b488
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2012-02-15 23:35:55 +0000

    *** empty log message ***

---
+++ backupsManagement.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+#######################################################
+# Handles backups as pickle files and such            #
+#                                                     #
+# Alden Stradling (Alden.Stradling@cern.ch) 18 Feb 10 #
+#######################################################
+
+import os, sys
+from controllerSettings import *
+
+hbList = [i for i in os.listdir(hotBackupPath) if volatileCSVName in i or volatileSQLName in i][:hotBackups]
+lbList = [i for i in os.listdir(longBackupPath) if i.endswith('.gz')][:keptBackups]
+
+for i in hbList:
+	os.system('rm %s' % hotBackupPath + i)
+for i in lbList:
+	os.system('rm %s' % longBackupPath + i)
+
+
