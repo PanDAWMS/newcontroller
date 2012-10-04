d2cdfc95dbf2fd6f23985c88c697e70ba0ab55bc
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2012-10-04 21:52:49 +0000

    Logic fix in DB pop

--- transferCosts.py
+++ transferCosts.py
@@ -68,9 +68,9 @@ def updateTransferCostsDB(addList, deleteList):
 	utils.initDB()
 	print "Init DB"
 
-	for i in addList:
+	for n,costDict in enumerate(addList):
 		keys = ','.join(headers)
-		values = [addList[i] for i in headers]
+		values = [costDict[n] for i in headers]
 		values[headers['timestamp']] = "TO_TIMESTAMP(" + values[headers['timestamp']] + ", 'YYYY-MM-DD_HH24:MI:SS')"
 		values = ','.join(values)
 		sql = 'INSERT INTO atlas_pandameta.transfercosts (%s) VALUES (%s)'
