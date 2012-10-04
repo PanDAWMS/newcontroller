9e95a94481aa0203c39ebf49a350025b05e954fc
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2012-10-04 22:02:31 +0000

    Fixed SQL

--- trunk/transferCosts.py
+++ trunk/transferCosts.py
@@ -77,7 +77,7 @@ for n,costDict in enumerate(addList):
 	values = [costDict[i] for i in headers]
 	values[headers.index('timestamp')] = "TO_TIMESTAMP(" + values[headers.index('timestamp')] + ", 'YYYY-MM-DD_HH24:MI:SS')"
 	values = ','.join(values)
-	sql = 'INSERT INTO atlas_pandameta.transfercosts (%s) VALUES (%s)'
+	sql = 'INSERT INTO atlas_pandameta.transfercosts (%s) VALUES (%s)' % (keys, values)
 	utils.dictcursor().execute(sql)
 utils.commit()
 
