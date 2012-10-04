391536a1992faeaa02391fabf6406226327d4018
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2012-10-04 17:35:55 +0000

    Initial costs table debugging

---
+++ trunk/costTable.py
@@ -0,0 +1,16 @@
+##########################################################################################
+# Tools for handling the transfer costs table in newController                           #
+#                                                                                        #
+# Alden Stradling 03 Oct 2012                                                            #
+# Alden.Stradling@cern.ch                                                                #
+##########################################################################################
+
+import os
+from SchedulerUtils import utils
+
+from miscUtils import *
+from controllerSettings import *
+from dbAccess import *
+
+#----------------------------------------------------------------------#
+#----------------------------------------------------------------------#
