e5ec1ff3460c9a2f75c9ecd538fa9150f9ac44aa
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2012-11-02 23:03:32 +0000

    Fixing AGIS population of major_release into release

--- trunk/swHandling.py
+++ trunk/swHandling.py
@@ -103,12 +103,13 @@ def updateInstalledSW(confd,lcgdict):
 				sw_agis[index] = {'siteid':release['panda_resource'],'cloud':release['cloud'],'release':release['major_release'],'cache':release['project']+'-'+release['release'],'cmtConfig':release['cmtconfig'].replace('unset in BDII',''),'validation':'AGIS'}
 
 			# For the releases
-			index = '%s_%s_%s_%s' % (release['panda_resource'],release['project']+'-'+release['major_release'],'',release['cmtconfig'].replace('unset in BDII',''))
-			sw_agis[index] = {'siteid':release['panda_resource'],'cloud':release['cloud'],'release':release['project']+'-'+release['major_release'],'cache':'None','cmtConfig':release['cmtconfig'].replace('unset in BDII',''),'validation':'AGIS'}
+			index = '%s_%s_%s_%s' % (release['panda_resource'],release['major_release'],'',release['cmtconfig'].replace('unset in BDII',''))
+			sw_agis[index] = {'siteid':release['panda_resource'],'cloud':release['cloud'],'release':release['major_release'],'cache':'None','cmtConfig':release['cmtconfig'].replace('unset in BDII',''),'validation':'AGIS'}
+
 		# Handling conditions correctly
 		else:
-			index = '%s_%s_%s_%s' % (release['panda_resource'],release['major_release'],release['release'],release['cmtconfig'].replace('unset in BDII',''))
-			sw_agis[index] = {'siteid':release['panda_resource'],'cloud':release['cloud'],'release':release['major_release'],'cache':release['project']+'-'+release['release'],'cmtConfig':release['cmtconfig'].replace('unset in BDII',''),'validation':'AGIS'}
+			index = '%s_%s_%s_%s' % (release['panda_resource'],release['major_release'],'',release['cmtconfig'].replace('unset in BDII',''))
+			sw_agis[index] = {'siteid':release['panda_resource'],'cloud':release['cloud'],'release':release['major_release'],'cache':'','cmtConfig':release['cmtconfig'].replace('unset in BDII',''),'validation':'AGIS'}
 			
 	# For CVMFS
 	for site in agissites:
