80a911fbaaeddbf70e11aafd9e732fab29715502
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2013-11-04 21:24:21 +0000

    Adding run scripts to SVN

---
+++ runSW.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+export PYTHONPATH=$PYTHONPATH:/data/atlpan/oracle/panda/monitor:/data/atlpan/panda/schedconfigProd:/data/atlpan/panda/schedconfigProd/autopilot:/data/atlpan/oracle/panda/monitor:/usr/lib/python2.5/site-packages
+export LOCKFILE=/data/atlpan/panda/.swLock
+export BASEPATH=/data/atlpan/panda/schedconfigProd
+# Check the lock:
+if [ ! -e $LOCKFILE ]
+  then
+    :> $LOCKFILE
+    :> ${LOCKFILE}.${1} 
+  else
+    echo "Schedconfig update blocked by lockfile on host $HOSTNAME"|mail -s "** Schedconfig Update Blocked -- Lockfile **" schedconfig@gmail.com
+    exit 10
+fi
+
+# Need grid setup
+#source /afs/cernch/project/gd/LCG-share/current_3.2/etc/profile.d/grid-env.sh
+
+echo "Setting panda dbtype to Oracle...."
+export PANDA_DBTYPE=oracle
+
+cd $BASEPATH/newController
+svn update --accept theirs-full
+python2.5 newController.py --sw
+
+rm -rf $LOCKFILE*
