a84fd34fe886104134146c519e7fca175efeac63
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2014-07-14 12:30:57 +0000

    Lockfile off for the nonce

--- runscripts/runProd.sh
+++ runscripts/runProd.sh
@@ -4,15 +4,15 @@ export LOCKPATH=/afs/cern.ch/user/a/atlpan/public/schedconfig_dont_touch
 export LOCKFILE=$LOCKPATH/.schedconfigLock
 export BASEPATH=/data/atlpan/panda/prod
 # Check the lock:
-echo ${LOCKFILE}.${1}
-if [ ! -e $LOCKFILE ]
-  then
-    :> $LOCKFILE
-    :> ${LOCKFILE}.${1} 
-  else
-    echo "Schedconfig update blocked by lockfile on host $HOSTNAME"|mail -s "** Schedconfig Update Blocked -- Lockfile **" schedconfig@gmail.com
-    exit 10
-fi
+#echo ${LOCKFILE}.${1}
+#if [ ! -e $LOCKFILE ]
+#  then
+#    :> $LOCKFILE
+#    :> ${LOCKFILE}.${1} 
+#  else
+#    echo "Schedconfig update blocked by lockfile on host $HOSTNAME"|mail -s "** Schedconfig Update Blocked -- Lockfile **" schedconfig@gmail.com
+#    exit 10
+#fi
 
 # Need grid setup
 #source /afs/cern.ch/project/gd/LCG-share/current_3.2/etc/profile.d/grid-env.sh
