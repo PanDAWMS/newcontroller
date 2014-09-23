fd4f2582ed5c9e0325fb0210101dada44cd02f33
stradlin <stradlin@22be200c-5e0a-4a8a-bf63-ee98bd1ebdf0>
2014-09-23 19:21:08 +0000

    Removed lockfile mechanism

--- runscripts/runMulticloud.sh
+++ runscripts/runMulticloud.sh
@@ -3,14 +3,6 @@ export PYTHONPATH=$PYTHONPATH:/data/atlpan/oracle/panda/monitor:/data/atlpan/pan
 export LOCKPATH=/afs/cern.ch/user/a/atlpan/public/schedconfig_dont_touch
 export LOCKFILE=$LOCKPATH/.netLock
 export BASEPATH=/data/atlpan/panda/prod
-# Check the lock:
-if [ ! -e $LOCKFILE ]
-  then
-    :> $LOCKFILE
-  else
-    echo "Net update blocked by lockfile on host $HOSTNAME"|mail -s "** Net Update Blocked -- Lockfile **" schedconfig@gmail.com
-    exit 10
-fi
 
 
 # Need grid setup
@@ -23,4 +15,3 @@ cd $BASEPATH/newController
 svn update
 python2.6 newController.py --multicloud
 
-rm -rf $LOCKFILE
--- runscripts/runProd.sh
+++ runscripts/runProd.sh
@@ -4,14 +4,14 @@ export LOCKPATH=/afs/cern.ch/user/a/atlpan/public/schedconfig_dont_touch
 export LOCKFILE=$LOCKPATH/.schedconfigLock
 export BASEPATH=/data/atlpan/panda/prod
 # Check the lock:
-#echo ${LOCKFILE}.${1}
-#if [ ! -e $LOCKFILE ]
-#  then
-#    :> $LOCKFILE
-#    :> ${LOCKFILE}.${1} 
-#  else
-#    echo "Schedconfig update blocked by lockfile on host $HOSTNAME"|mail -s "** Schedconfig Update Blocked -- Lockfile **" schedconfig@gmail.com
-#    exit 10
+echo ${LOCKFILE}.${1}
+if [ ! -e $LOCKFILE ]
+  then
+    :> $LOCKFILE
+    :> ${LOCKFILE}.${1} 
+  else
+    echo "Schedconfig update blocked by lockfile on host $HOSTNAME"|mail -s "** Schedconfig Update Blocked -- Lockfile **" schedconfig@gmail.com
+    exit 10
 #fi
 
 # Need grid setup
